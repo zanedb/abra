0bd29b9ca1c64025951b7d8823759910edfa39cd
Zane <github@zane.sh>
2023-09-24 01:58:05 -0700

    idk if this is relevant commit it lol i did all this shit a month ago and forgot about it and now im gonna do more so i might as well commit what i have

--- abra/Models/Stream.swift
+++ abra/Models/Stream.swift
@@ -13,14 +13,22 @@ extension SStream: MKAnnotation {
     public var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
     }
+    
+    // for annotation callout view
+    public var title: String? {
+        return ""
+    }
+    
+    // see https://www.timekl.com/blog/2017/04/02/putting-core-data-on-the-map/
+    class func keyPathsForValuesAffectingCoordinate() -> Set<String> {
+        return Set<String>([ #keyPath(latitude), #keyPath(longitude) ])
+    }
 }
 
 
 // TODO: create .artwork property that returns artwork view
 
 extension SStream {
-    
-    
     static var example: SStream {
         let viewContext: NSManagedObjectContext = PersistenceController.preview.container.viewContext
         let newItem = SStream(context: viewContext)
