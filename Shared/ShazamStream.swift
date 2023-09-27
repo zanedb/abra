4943552b9d6b3614f956422b7bfa935635b80187
Zane <github@zane.sh>
2023-09-26 23:12:21 -0700

    add date extensions & ShazamStream extensions for sections; forgot to commit

--- Shared/ShazamStream.swift
+++ Shared/ShazamStream.swift
@@ -45,14 +45,76 @@ import MapKit
     }
 }
 
+extension Date {
+    func isInLastSevenDays() -> Bool {
+        let now = Date()
+        guard let aWeekAgo = Calendar.current.date(byAdding: .weekOfYear, value: -1, to: now) else { return false }
+        return self <= now && self > aWeekAgo
+    }
+    
+    func isInLastThirtyDays() -> Bool {
+        let now = Date()
+        guard let thirtyDaysAgo = Calendar.current.date(byAdding: .day, value: -30, to: now) else { return false }
+        return self <= now && self > thirtyDaysAgo
+    }
+    
+    var month: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM"
+        return dateFormatter.string(from: self)
+    }
+    
+    func relativeGroupString() -> String {
+        if (Calendar.current.isDateInToday(self)) {
+            return "Today"
+        } else if(self.isInLastSevenDays()) {
+            return "Last 7 Days"
+        } else if(self.isInLastThirtyDays()) {
+            return "Last 30 Days"
+        } else {
+            return self.month
+        }
+    }
+}
+
 extension ShazamStream {
     public var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
     }
     
+    public var relativeTime: String {
+        if (timestamp.isInLastThirtyDays()) {
+            RelativeDateTimeFormatter().localizedString(for: timestamp, relativeTo: Date.now)
+        } else {
+            timestamp.formatted(.dateTime.day().month().hour().minute())
+        }
+    }
+    
+    public var definiteDate: String {
+        if (Calendar.current.isDateInToday(timestamp)) {
+            "Today"
+        } else if (Calendar.current.isDateInYesterday(timestamp)) {
+            "Yesterday"
+        } else {
+            timestamp.formatted(.dateTime.weekday().day().month())
+        }
+    }
+    
+    public var definiteDateAndTime: String {
+        timestamp.formatted(.dateTime.day().month().hour().minute())
+    }
+    
+    public var timeGroupedString: String {
+        timestamp.relativeGroupString()
+    }
+    
+    public var placeGroupedString: String {
+        thoroughfare ?? "Unknown"
+    }
+    
     static var preview: ShazamStream {
         ShazamStream(title: "The Ills", artist: "Denzel Curry", isExplicit: true,
                      artworkURL: URL(string: "https://upload.wikimedia.org/wikipedia/en/c/cf/Denzel_Curry_-_Melt_My_Eyez_See_Your_Future.png")!,
-                     timestamp: .now, latitude: 37.721941, longitude: -122.4739084)
+                     latitude: 37.721941, longitude: -122.4739084)
     }
 }
