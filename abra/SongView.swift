da698434eaa6e6dab59fc29a16e3287e2c98812b
Zane <github@zane.sh>
2024-10-04 20:36:28 -0700

    Add custom toolbar

--- abra/SongView.swift
+++ abra/SongView.swift
@@ -12,6 +12,7 @@ import SwiftData
 
 struct SongView: View {
     @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.dismiss) private var dismiss
     @EnvironmentObject private var vm: ViewModel
     @EnvironmentObject private var library: LibraryService
     
@@ -19,6 +20,8 @@ struct SongView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
+            toolbar
+            
             card
             
             if (!library.hasIgnoredPhotosRequest) {
@@ -28,8 +31,30 @@ struct SongView: View {
                     .padding(.bottom, 8)
                 Photos(stream: stream)
             }
+            
+            Spacer()
         }
             .padding()
+    }
+    
+    var toolbar: some View {
+        HStack(alignment: .center) {
+            ShareLink(item: stream.appleMusicURL!) {
+                Label("Share", systemImage: "square.and.arrow.up")
+                    .labelStyle(.iconOnly)
+                    .font(.system(size: 20))
+            }
+            
+            Spacer()
+            
+            Button { dismiss() } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 32))
+                    .symbolRenderingMode(.hierarchical)
+            }
+        }
+            .padding(.bottom, 12)
     }
     
     var card: some View {
