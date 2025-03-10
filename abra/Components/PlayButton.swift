eaee55d1b33b1138dbacc3e191e60476cbc1890d
Zane <github@zane.sh>
2025-03-10 15:38:11 -0700

    PlayButton: handle errors, play in Music, and show state

--- abra/Components/PlayButton.swift
+++ abra/Components/PlayButton.swift
@@ -9,14 +9,55 @@ import SwiftUI
 import MusicKit
 
 struct PlayButton: View {
+    @Environment(\.openURL) private var openURL
     @StateObject var music = MusicController.shared.music
+    @State private var alertShown = false
     
     var appleMusicID: String
     
     var body: some View {
-        Button(action: { music.play(id: appleMusicID)}) {
-            Label("Listen", systemImage: "play.fill")
+        Button(action: {
+            if music.errorMessage != nil {
+                return alertShown = true
+            }
+            
+            if music.isPlaying {
+                music.stopPlayback()
+                
+                // If the PlayButton is on a different SongView, start new playback
+                if music.currentTrackID != appleMusicID {
+                    music.play(id: appleMusicID)
+                }
+            } else {
+                music.play(id: appleMusicID)
+            }
+        }) {
+            if (music.currentTrackID == appleMusicID) && music.isPlaying {
+                Label("Pause", systemImage: "pause.fill")
+            } else {
+                Label("Play", systemImage: "play.fill")
+            }
         }
+            .onAppear {
+                Task {
+                    let status = await music.authorize()
+                    if status != .authorized {
+                        DispatchQueue.main.async {
+                            music.errorMessage = "Not authorized to access Apple Music"
+                        }
+                    }
+                }
+            }
+            .alert(music.errorMessage ?? "Something went wrong.", isPresented: $alertShown) {
+                // If it's a permissions issue, include a handy button to jump right there!
+                if (music.errorMessage != nil) && music.errorMessage!.contains("Not authorized") {
+                    Button("Grant Access") {
+                        openURL(URL(string: UIApplication.openSettingsURLString)!)
+                    }
+                }
+                
+                Button("Close", role: .cancel) { }
+            }
     }
 }
 
