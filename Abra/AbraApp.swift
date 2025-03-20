7cda8d8fe752f70b6cf21a9a29d0b1b2009c57ac
Zane <github@zane.sh>
2025-03-19 21:43:06 -0700

    Stop recording if minimized w/o active Activity

--- Abra/AbraApp.swift
+++ Abra/AbraApp.swift
@@ -23,7 +23,13 @@ struct abraApp: App {
                 }
                 .environmentObject(vm)
                 .environmentObject(library)
+                .onChange(of: scenePhase) {
+                    // If app is minimized and no session is active, stop recording
+                    if (scenePhase == .inactive && !vm.isActivityRunning) {
+                        vm.stopRecording()
+                    }
+                }
         }
-        .modelContainer(for: ShazamStream.self)
+            .modelContainer(for: ShazamStream.self)
     }
 }
