f3126cb0e4d368377c1d7e7d944aeffc1a0b9070
Zane <github@zane.sh>
2025-04-30 03:23:29 -0700

    Someday

--- Abra/AbraApp.swift
+++ Abra/AbraApp.swift
@@ -3,13 +3,29 @@
 //  Abra
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct abraApp: App {
+//    let database: Database
+//    
+//    init() {
+//        let schema = Schema([ShazamStream.self, Place.self])
+//        let modelConfiguration = ModelConfiguration(schema: schema)
+//        
+//        do {
+//            let container = try ModelContainer(for: schema, configurations: modelConfiguration)
+//            database = Database(container: container)
+//        } catch {
+//            fatalError("Failed to create modelContainer: \(error.localizedDescription)")
+//        }
+//    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+//                .environment(\.database, database)
         }
             .modelContainer(for: ShazamStream.self)
     }
