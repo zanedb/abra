accb87d676d85ac60660f756e8c8f0ee5cca81fc
Zane <github@zane.sh>
2025-05-27 14:11:55 -0700

    Replace custom search bar with UISearchBar

--- Abra/Components/SearchBar.swift
+++ Abra/Components/SearchBar.swift
@@ -5,80 +5,36 @@
 
 import SwiftUI
 
-struct SearchBar: View {
-    @Environment(\.colorScheme) private var colorScheme
-    @Environment(ShazamProvider.self) private var shazam
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String
     
-    var prompt: String = "Search…"
-    @Binding var search: String
-    @FocusState var focused: Bool
-    
-    var body: some View {
-        HStack(alignment: .center) {
-            TextField(prompt, text: $search, prompt:
-                Text(prompt)
-                    .foregroundColor(.gray)
-            )
-                .padding(7)
-                .padding(.leading, 24)
-                .background(.gray.opacity(colorScheme == .light ? 0.13 : 0.20))
-                .cornerRadius(9)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 7)
-                        if (search != "") {
-                            Button(action: {
-                                self.search = ""
-                            }) {
-                                Image(systemName: "multiply.circle.fill")
-                                    .foregroundColor(.gray)
-                                    .padding(.trailing, 6)
-                            }
-                        }
-                    }
-                )
-                .focused($focused)
-            
-            if (focused || (!focused && search != "")) {
-                Button(action: {
-                    focused = false
-                    self.search = ""
-                }) {
-                    Text("Cancel")
-                        .font(.system(size: 17))
-                }
-                    .padding(.leading, 3)
-                    .transition(.asymmetric(
-                        insertion: .opacity.animation(.easeInOut(duration: 0.2)),
-                        removal: .opacity.animation(.easeInOut(duration: 0.01))
-                    ))
-            } else {
-                Button(action: { Task { await shazam.startMatching() } }) {
-                    Image(systemName: "shazam.logo.fill")
-                        .symbolRenderingMode(.multicolor)
-                        .tint(.blue)
-                        .fontWeight(.medium)
-                        .font(.system(size: 36))
-                        .cornerRadius(100)
-                }
-                    .padding(.leading, -3)
-            }
-        }
-            .padding(.top, focused ? 3.2 : 0)
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = placeholder
+        searchBar.autocapitalizationType = .none
+        return searchBar
     }
-}
-
-#Preview {
-    NavigationStack {
-        SearchBar(search: .constant(""), focused: FocusState())
-            .padding()
-        List {
-            
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var text: String
+        
+        init(text: Binding<String>) {
+            _text = text
         }
-        .listStyle(.plain)
-        .searchable(text: .constant(""), placement: .toolbar, prompt: "Search Shazams…")
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
     }
 }
