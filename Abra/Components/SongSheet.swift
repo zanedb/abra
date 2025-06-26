13d1784b1ac93191be53c582173b4a4e2afbca74
Zane <github@zane.sh>
2025-06-26 13:30:52 -0700

    Tweak frame width on minimization

--- Abra/Components/SongSheet.swift
+++ Abra/Components/SongSheet.swift
@@ -10,10 +10,10 @@ struct SongSheet: View {
     @Environment(\.colorScheme) var colorScheme
     
     var stream: ShazamStream
-    var mini: Bool? = false
+    var mini: Bool = false
     
     private var imageSize: CGFloat {
-        mini ?? false ? 48 : 96
+        mini ? 48 : 96
     }
     
     var body: some View {
@@ -25,6 +25,7 @@ struct SongSheet: View {
                 .frame(width: imageSize, height: imageSize)
                 .cornerRadius(3.0)
                 .padding(.trailing, 5)
+                .padding(.leading, mini ? 4 : 0)
             
             VStack(alignment: .leading, spacing: 0) {
                 Text(stream.title)
@@ -32,11 +33,13 @@ struct SongSheet: View {
                     .font(.system(size: 18))
                     .padding(.bottom, 2)
                     .lineLimit(2)
-                    .frame(maxWidth: 180, alignment: .leading)
+                    .frame(maxWidth: mini ? 220 : 180, alignment: .leading)
                 Text(stream.artist)
                     .foregroundColor(.gray)
                     .font(.system(size: 15))
                     .padding(.bottom, 3)
+                    .lineLimit(2)
+                    .frame(maxWidth: mini ? 180 : 220, alignment: .leading)
                 
                 Spacer()
                 
