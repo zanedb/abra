0d5632849dd241b266924c17edd4ffeee9891ce8
Zane <github@zane.sh>
2025-07-15 18:12:12 -0700

    Rendering modes on SpotIcon

--- Abra/Components/SpotIcon.swift
+++ Abra/Components/SpotIcon.swift
@@ -9,24 +9,34 @@ struct SpotIcon: View {
     var symbol: String
     var color: Color
     var size: CGFloat = 40
-    
+    var renderingMode: RenderingMode = .plain
+
+    enum RenderingMode {
+        case plain
+        case hierarchical
+        case iconOnly
+    }
+
     var body: some View {
         Image(systemName: symbol == "" ? "plus.circle.fill" : symbol)
             .resizable()
             .scaledToFit()
             .frame(width: size / 2, height: size / 2)
-            .foregroundColor(.white)
+            .foregroundColor(renderingMode == .iconOnly || renderingMode == .hierarchical ? color : .white)
             .frame(width: size, height: size)
-            .background(symbol == "" ? .gray.opacity(0.20) : color)
+            .background(
+                renderingMode == .iconOnly
+                    ? .clear
+                    : (symbol == "" ? .gray.opacity(0.20) : (renderingMode == .hierarchical ? color.opacity(0.15) : color))
+            )
             .clipShape(Circle())
     }
 }
 
 #Preview {
     HStack {
-        SpotIcon(symbol: "key.fill", color: .red, size: 144)
-        SpotIcon(symbol: "plus.circle.fill", color: .red, size: 144)
-        SpotIcon(symbol: "bicycle", color: .red, size: 80)
+        SpotIcon(symbol: "plus.circle.fill", color: .red, size: 144, renderingMode: .iconOnly)
+        SpotIcon(symbol: "bicycle", color: .red, size: 80, renderingMode: .hierarchical)
         SpotIcon(symbol: "play", color: .red)
     }
 }
