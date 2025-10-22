f94a15a96c7e393e1a359a5cffa45e5d81f6b1c9
Zane <github@zane.sh>
2025-10-22 11:23:51 -0700

    Glassy inspector

--- Abra/Extensions/View+PresentationInspector.swift
+++ Abra/Extensions/View+PresentationInspector.swift
@@ -5,18 +5,22 @@
 
 import SwiftUI
 
-struct PresentationInspector: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .presentationBackgroundInteraction(.enabled(upThrough: .fraction(0.50)))
-            .presentationBackground(.thickMaterial)
-            .presentationCornerRadius(18)
-    }
-}
-
+// Allows background interaction on medium detent (0.50)
+// On iOS 18: background to .thickMaterial, corner radius to 18
 extension View {
-    /// Allows background interaction, sets the background to .thickMaterial, and sets preferred corner radius (18).
-    func presentationInspector() -> some View {
-        modifier(PresentationInspector())
+    @ViewBuilder func presentationInspector() -> some View {
+        if #available(iOS 26.0, *) {
+            self
+                .presentationBackgroundInteraction(
+                    .enabled(upThrough: .fraction(0.50))
+                )
+        } else {
+            self
+                .presentationBackgroundInteraction(
+                    .enabled(upThrough: .fraction(0.50))
+                )
+                .presentationBackground(.thickMaterial)
+                .presentationCornerRadius(18)
+        }
     }
 }
