2cde7d5c3ae66dffd537c83a93ccb42e40e4725b
Zane <github@zane.sh>
2025-11-26 14:11:36 -0800

    Remove unused db code

--- Abra/AbraApp.swift
+++ Abra/AbraApp.swift
@@ -3,25 +3,12 @@
 //  Abra
 //
 
+import Sentry
 import SwiftData
 import SwiftUI
 
 @main
 struct abraApp: App {
-//    let database: Database
-//    
-//    init() {
-//        let schema = Schema([ShazamStream.self, Place.self])
-//        let modelConfiguration = ModelConfiguration(schema: schema)
-//        
-//        do {
-//            let container = try ModelContainer(for: schema, configurations: modelConfiguration)
-//            database = Database(container: container)
-//        } catch {
-//            fatalError("Failed to create modelContainer: \(error.localizedDescription)")
-//        }
-//    }
-
     init() {
         ValueTransformer.setValueTransformer(UIColorValueTransformer(), forName: NSValueTransformerName("UIColorValueTransformer"))
     }
