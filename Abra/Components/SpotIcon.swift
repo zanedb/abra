7e22b125afcb88127b5db87261ef4ad57198861b
Zane <github@zane.sh>
2025-11-26 14:11:11 -0800

    Gradient on Spot icon

--- Abra/Components/SpotIcon.swift
+++ Abra/Components/SpotIcon.swift
@@ -22,21 +22,56 @@ struct SpotIcon: View {
             .resizable()
             .scaledToFit()
             .frame(width: size / 2, height: size / 2)
-            .foregroundStyle(renderingMode == .iconOnly || renderingMode == .hierarchical ? color : .white)
+            .foregroundStyle(
+                renderingMode == .iconOnly || renderingMode == .hierarchical
+                    ? LinearGradient(
+                        colors: [
+                            color.opacity(0.7), color.opacity(1),
+                            color.opacity(1),
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottomTrailing
+                    )
+                    : LinearGradient(
+                        colors: [.white],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+            )
             .frame(width: size, height: size)
             .background(
-                renderingMode == .iconOnly
-                    ? .clear
-                    : (symbol == "" ? .gray.opacity(0.20) : (renderingMode == .hierarchical ? color.opacity(0.15) : color))
+                LinearGradient(
+                    colors: (renderingMode == .iconOnly
+                        ? [.clear]
+                        : (symbol.isEmpty
+                            ? [.gray.opacity(0.2)]
+                            : (renderingMode == .hierarchical
+                                ? [color.opacity(0.15)]
+                                : [
+                                    color.opacity(0.7), color.opacity(1),
+                                    color.opacity(1),
+                                ]))),
+                    startPoint: .top,
+                    endPoint: .bottomTrailing
+                )
             )
             .clipShape(Circle())
     }
 }
 
-#Preview {
+#Preview("SpotIcon") {
     HStack {
-        SpotIcon(symbol: "plus.circle.fill", color: .red, size: 144, renderingMode: .iconOnly)
-        SpotIcon(symbol: "bicycle", color: .red, size: 80, renderingMode: .hierarchical)
+        SpotIcon(
+            symbol: "plus.circle.fill",
+            color: .red,
+            size: 144
+        )
+        SpotIcon(
+            symbol: "bicycle",
+            color: .red,
+            size: 80,
+            renderingMode: .hierarchical
+        )
         SpotIcon(symbol: "play", color: .red)
     }
 }
