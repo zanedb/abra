9affe2df4d91fc3daeef5e090079bddbb0332db0
Zane <github@zane.sh>
2025-11-26 15:04:02 -0800

    Remove database ModelActor

--- Abra/AbraApp.swift
+++ Abra/AbraApp.swift
@@ -10,13 +10,22 @@ import SwiftUI
 @main
 struct abraApp: App {
     init() {
-        ValueTransformer.setValueTransformer(UIColorValueTransformer(), forName: NSValueTransformerName("UIColorValueTransformer"))
+        ValueTransformer.setValueTransformer(
+            UIColorValueTransformer(),
+            forName: NSValueTransformerName("UIColorValueTransformer")
+        )
+
+        SentrySDK.start { options in
+            options.dsn =
+                "https://d336ddac8a50dbb29910b3384c913606@o4504745853321216.ingest.us.sentry.io/4509637227773952"
+            options.debug = false
+            options.sendDefaultPii = true
+        }
     }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-//                .environment(\.database, database)
         }
         .modelContainer(for: [ShazamStream.self, Spot.self])
     }
