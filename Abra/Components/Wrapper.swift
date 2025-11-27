71771464fb466ff4f75c0082f54555ee3c6cb2d7
Zane <github@zane.sh>
2025-11-26 20:34:37 -0800

    Wrapper preview

--- Abra/Components/Wrapper.swift
+++ Abra/Components/Wrapper.swift
@@ -4,6 +4,7 @@
 //
 
 import SwiftUI
+import _MapKit_SwiftUI
 
 struct Wrapper<Content: View>: View {
     let content: Content
@@ -24,3 +25,24 @@ struct Wrapper<Content: View>: View {
     }
 }
 
+
+#Preview {
+    Map()
+        .sheet(isPresented: .constant(true)) {
+            VStack {
+                Wrapper {
+                    HStack {
+                        Image(systemName: "list.bullet.indent")
+                        Text("3 songs by Denzel Curry in library.")
+                    }
+                    .font(.callout)
+                    .frame(height: 50)
+                }
+                Spacer()
+            }
+            .presentationDetents([.fraction(0.50), .large])
+            .presentationInspector()
+            .edgesIgnoringSafeArea(.bottom)
+            .prefersEdgeAttachedInCompactHeight()
+        }
+}
